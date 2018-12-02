615f1086fbcbe0d73726a2564490a8eec4ad2c5f
Kamil Radomski <kamilmarekradomski@gmail.com>
2018-12-02 16:11:02 +0100

    exercise from lesson 8th plus extra exercises

---
+++ 8/o_sobie1.py
@@ -0,0 +1,28 @@
+my_dict = {
+	'name': 'Kamil',
+	'shoe number': 32
+}
+
+pytanie = input('Zadaj mi pytanie\n')
+about_me = my_dict.get(pytanie)
+# 1. about_me ma wartość
+# 2. nie ma takiego klucza i wtedy about_me jest None
+
+while pytanie != 'koniec':
+
+	if about_me:
+		print(about_me)
+	elif 'admin123' in pytanie:
+		new_key = input('Podaj nowy klucz')
+		my_dict[new_key] = input('podaj nowe dane')
+		print(my_dict.get(new_key) + ' dodano do słownika')
+	else: 
+		print('To są dane poufne')
+
+	pytanie = input('Zadaj mi pytanie\n')
+	about_me = my_dict.get(pytanie)
+
+
+for k, v in my_dict.items():
+	print('My {} is {}'.format(k, v))
+
